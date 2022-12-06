4fb8cc4ce93e52f34135ac1605ba27d4133cadce
Vasilis Vrettos <billvrettos2000@gmail.com>
2022-12-07 00:47:46 +0200

    Lab3: Only critical/atomic missing, starting Lab4

--- Lab3/plots/kmeansLockPlotter.py
+++ Lab3/plots/kmeansLockPlotter.py
@@ -4,9 +4,9 @@ import sys
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from pathlib import Path
 
-
-def logger(filename:str) -> list:
+def logger(filename:str) -> list[float]:
     kmeansTime = []
     kmeansSpeedup = []
     
@@ -27,14 +27,47 @@ def logger(filename:str) -> list:
 
 def parser() -> str:
     parser = argparse.ArgumentParser(description = "Kmeans Lock Plotter")
-    parser.add_argument('--locktype', help = 'Select Locktype', type = str, choices = ['nosync', 'mutex', 'spinlock', 'tas', 'ttas', 'array', 'clh', 'all', 'critical', 'atomic'], required=True)
-    parser.add_argument('--outfile', help = 'Kmeans outfile path (relative to script)', type = str, required=True)
-    parser.add_argument('--affinity', type = str, default="")
+    parser.add_argument('--locktype', help = 'Select Locktype', type = str, choices = ['nosync', 'pthread_mutex', 'pthread_spin', 'tas', 'ttas', 'array', 'clh', 'all', 'critical', 'atomic'], required=True)
+    parser.add_argument('--folder', type = str, default="outFiles")
     namespace = parser.parse_args()
 
     return namespace
 
-def timePlotter(outFilePath:str, title:str, timeList, color:str):
+def plotMultiple(outFilePath:str, title:str, inputList, color:list[str], yTitle:str, mode:str):
+    threads = ['1','2','4','8','16','32','64']
+    legend = ['No Sync', 'PThread-Mutex', 'PThread-Spinlock', 'TAS', 'TTAS', 'Array', 'CLH']
+
+    f = plt.figure()
+    f.set_figwidth(8)
+    f.set_figheight(6)
+    f.tight_layout()
+
+    X_axis = np.arange(len(threads))
+
+    if mode == 'Time':
+        cnt = 0.05
+        for idx, i in enumerate(inputList):
+            plt.bar(X_axis + cnt, i, 0.1, align='edge', label=legend[idx], color=color[idx])
+            cnt += 0.1
+        plt.xticks(X_axis + 0.3, threads)
+        plt.xlabel("Number of Threads")
+        plt.ylabel(yTitle)
+        plt.title(title)
+        plt.legend()
+        plt.savefig(outFilePath)
+    elif mode == 'Speedup':
+        cnt = 0.05
+        for idx, i in enumerate(inputList):
+            plt.plot(X_axis + cnt, i, 0.1, marker='o', label=legend[idx], color=color[idx])
+        plt.xticks(X_axis + 0.3, threads)
+        plt.xlabel("Number of Threads")
+        plt.ylabel(yTitle)
+        plt.title(title)
+        plt.legend()
+        plt.savefig(outFilePath)
+
+
+def timePlotter(outFilePath:str, title:str, timeList, color:str) -> list[float]:
     threads = ['1','2','4','8','16','32','64']
 
     f = plt.figure()
@@ -52,7 +85,7 @@ def timePlotter(outFilePath:str, title:str, timeList, color:str):
     plt.title(title)
     plt.savefig(outFilePath)
     
-def speedupPlotter(outFilePath:str, title:str, speedupList, color:str):
+def speedupPlotter(outFilePath:str, title:str, speedupList, color:str) -> list[float]:
     threads = ['1','2','4','8','16','32','64']
 
     f = plt.figure()
@@ -74,30 +107,62 @@ def main():
     # Parse arguments
     namespace = parser()
     lock = namespace.locktype
-    affinity = namespace.affinity
+    outFolder = namespace.folder
+    plotFolder = f"{outFolder}/plots"
+    Path(plotFolder).mkdir(parents=True, exist_ok=True)
 
-    # Switch-Case (Python gamiesai)
-    folder = f"plots{affinity}"
-    timePlotPath = f"{folder}/kmeans_locks_{lock}.jpg"
-    speedupPlotPath = f"{folder}/kmeans_locks_{lock}_speedup.jpg"
+    # Choice Arrays
+    locks = ['No Lock', 'Mutex', 'Spinlock', 'TAS', 'TTAS', 'Array', 'CLH']
+    colors = ['red', 'green', 'purple', 'darkred', 'blue', 'orange', 'darkblue']
+    lockFiles = ['nosync', 'pthread_mutex', 'pthread_spin', 'tas', 'ttas', 'array', 'clh']
     
+
     if ((lock == 'critical') or (lock == 'atomic')):
+        color = 'black'
         timeTitle = f"K-Means - OMP {lock} - Size:16MB, Coords:16, Clusters:16"
         speedupTitle = f"K-Means - OMP {lock} - Speedup - Size:16MB, Coords:16, Clusters:16"
+    elif lock == "all":
+        index = 0
+        allTimeList = []
+        allSpeedupList = []
+        allTimeTitle = "K-Means - Time - Size:16MB, Coords:16, Clusters:16"
+        allSpeedupTitle = "K-Means - Speedup - Size:16MB, Coords:16, Clusters:16"
+        for i in locks:
+            timeList, speedupList = logger(f"{outFolder}/sandman_kmeans_{lockFiles[index]}.out")
+            allTimeList.append(timeList)
+            allSpeedupList.append(speedupList)
 
-    timeTitle = f"K-Means - {lock} Lock - Size:16MB, Coords:16, Clusters:16"
-    speedupTitle = f"K-Means - {lock} Lock - Speedup - Size:16MB, Coords:16, Clusters:16"
+            timeTitle = f"K-Means - {i} - Size:16MB, Coords:16, Clusters:16"
+            speedupTitle = f"K-Means - {i} - Speedup - Size:16MB, Coords:16, Clusters:16"
 
-    color = "red"
-    # Log Data from Outfiles
-    timeList, speedupList = logger(namespace.outfile)
+            timePlotPath = f"{plotFolder}/kmeans_locks_{lockFiles[index]}.jpg"
+            speedupPlotPath = f"{plotFolder}/kmeans_locks_{lockFiles[index]}_speedup.jpg"
+            color = colors[index]
 
-    # Plot and Save
-    print(timeList, speedupList)
-    speedupPlotter(outFilePath=speedupPlotPath, title=speedupTitle, speedupList=speedupList, color=color)
+            index += 1 
+            print (timeList, speedupList)
+            speedupPlotter(outFilePath=speedupPlotPath, title=speedupTitle, speedupList=speedupList, color=color)
+            timePlotter(outFilePath=timePlotPath, title=timeTitle, timeList=timeList, color=color)
+        plotMultiple(f"{plotFolder}/kmeans_locks_all.jpg", allTimeTitle, allTimeList, colors, "Time (in seconds)", mode='Time')
+        plotMultiple(f"{plotFolder}/kmeans_locks_all_speedup.jpg", allSpeedupTitle, allSpeedupList, colors, "Speedup", mode='Speedup')
 
-    timePlotter(outFilePath=timePlotPath, title=timeTitle, timeList=timeList, color=color)
+    else:
+        lockIndex = lockFiles.index(lock)
+        ## Get info for Plots
+        # Data
+        timeList, speedupList = logger(f"{outFolder}/sandman_kmeans_{lock}.out")
+        # Plot result path
+        timePlotPath = f"{plotFolder}/kmeans_locks_{lock}.jpg"
+        speedupPlotPath = f"{plotFolder}/kmeans_locks_{lock}_speedup.jpg"
+        # Titles for Plots
+        timeTitle = f"K-Means - {locks[lockIndex]} Lock - Size:16MB, Coords:16, Clusters:16"
+        speedupTitle = f"K-Means - {lock[lockIndex]} Lock - Speedup - Size:16MB, Coords:16, Clusters:16"
+        # Color
+        color = colors[lockIndex]
+        print(timeList, speedupList)
 
+        speedupPlotter(outFilePath=speedupPlotPath, title=speedupTitle, speedupList=speedupList, color=color)
+        timePlotter(outFilePath=timePlotPath, title=timeTitle, timeList=timeList, color=color)
 
 if __name__ == "__main__":
     main()
--- Lab3/plots/plot_all.sh
+++ Lab3/plots/plot_all.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# for affinity_type in "affinitymouliko" "cores" "coresclose" ""
-# do
-#     mkdir -p "plots${affinity_type}"
-#     for lock_type in "nosync" "mutex" "spinlock" "tas" "ttas" "array" "clh"
-#     do
-#         python3 kmeansLockPlotter.py --locktype ${lock_type} --outfile outFiles${affinity_type}/sandman_kmeans_${lock_type}.out
-#     done
-# done
+for folder in "outFiles" "outFilesAffinityMouliko" "outFilesCores" "outFilesCoresClose"
+do
+    python3 ./kmeansLockPlotter.py --locktype all --folder $folder
+done
 
